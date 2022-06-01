ed3ec119c6e5b183eabfa9e0d04e5cd942d5066f
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-01 22:56:49 +0200

    ajout option non lu

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,7 +5,6 @@ class EmailsController < ApplicationController
     end
 
     def create
-        puts "je suis dans create"
         @email = Email.new(
             object: Faker::Games::Witcher.quote,
             body: Faker::Lorem.sentence(word_count: 20)
@@ -19,11 +18,9 @@ class EmailsController < ApplicationController
     end
 
     def show
-        puts "je suis dans show"
         @email = Email.find(params[:id])
         respond_to do |format|
             format.html {
-                puts "je suis dans format html"
                 redirect_to root_path}
             format.js {}
         end
@@ -34,15 +31,12 @@ class EmailsController < ApplicationController
     end
 
     def update
-        puts "je suis dans update"
         @email = Email.find(params[:id])
         @email.update_attribute(:read, params[:read])
         respond_to do |format|
             format.html {
-                puts "je suis dans format html"
                 redirect_to root_path}
             format.js {
-                puts "je suis dans js"
             }
         end
     end
